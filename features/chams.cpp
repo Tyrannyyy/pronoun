d733633a087f2acfd87c217dd6758790ebc9e779
Tyrannyyy <46105966+Tyrannyyy@users.noreply.github.com>
2019-01-03 16:55:29 +0000

    Add files via upload

---
+++ features/chams.cpp
@@ -0,0 +1,9 @@
+#include "chams.hpp"
+#include <fstream>
+
+#include "../valve_sdk/csgostructs.hpp"
+#include "../options.hpp"
+#include "../hooks.hpp"
+#include "../helpers/input.hpp"
+
+//redo whole chams ( make scene end )
